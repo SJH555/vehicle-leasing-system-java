99fd4c4858ae3a8de14f2b3f8369098b7f80bcf1
SJH <isjhsong@163.com>
2024-05-17 19:49:55 +0800

    add: used association tags for vehicle info sql

---
+++ ruoyi-modules/rent-service/src/main/java/com/szxj/rent/domain/vo/RentVehicleVo.java
@@ -0,0 +1,22 @@
+package com.szxj.rent.domain.vo;
+
+import com.szxj.rent.domain.RentVehicle;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@EqualsAndHashCode(callSuper = false)
+@ToString(callSuper = true)
+@Data
+public class RentVehicleVo extends RentVehicle {
+
+    /** 品牌名称 */
+    private String brandName;
+
+    /** 品牌名称 */
+    private String modelName;
+
+      /** 颜色名称 */
+    private String colorName;
+
+}
